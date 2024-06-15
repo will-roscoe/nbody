8f9df318d1299647fb8376f0bb40ced36471d9cd
Will Roscoe <w.roscoe@lancaster.ac.uk>
2024-06-15 22:08:59 +0100

    Organising
    added comments, regulated spacing and added headings to space out parts of file. Removed clutter and notes and put in separate files

--- v3.0/base.py
+++ v3.0/base.py
@@ -9,16 +9,28 @@ import re
 from typing import List, Union
 import numpy as np
 import random
+
 from vispy import scene
 from vispy.scene import visuals
+##########################################################################################
+#                                       Particle class                                   #
+##########################################################################################
+
+
 class Particle:
-    def __init__(self, pos: np.ndarray, vel: np.ndarray, mass: float, radius: float, charge: float, color: Union[str, np.ndarray] = 'white'):
+    def __init__(self,
+                 pos: np.ndarray, vel: np.ndarray,
+                 mass: float, radius: float,
+                 charge: float, color: Union[str, np.ndarray] = 'white'):
+        
         self.pos = np.array(pos, dtype=float)
         self.vel = np.array(vel, dtype=float)
         self.mass = mass
         self.radius = radius
         self.charge = charge
         self.color = color
+    
+    
     def __repr__(self) -> str:
         return f"\nParticle(p={self.pos},v={self.vel},m={self.mass},r={self.radius},c={self.charge})"
 
@@ -49,62 +61,83 @@ class Engine:
                 dB = self.mu0/(4*np.pi) * (other.charge * other.vel * self.dt * np.cross(r, other.vel)) / r_mag**3
                 force += p.charge * np.cross(p.vel, dB)
         return force
+
+##########################################################################################
+#                                       Node class                                       #
+##########################################################################################
+
+
 class Node:
     def __init__(self, pos: np.ndarray, size: float, mass: float=0., depth=0) -> None:
-        self.pos = pos
-        self.size = size
-        self.mass = mass
+        self.pos = pos # center of node
+        self.size = size # each node is cubic so only need one size value
+        self.mass = mass # total mass of node, including children
         self.children = []  # child nodes
-        self.particles = []  # particles in this node
-        self.is_end = False
-        self.depth = depth
-        self.cmass = pos
+        self.particles = []  # particles directly in this node
+        self.is_end = False # is this a leaf node
+        self.depth = depth # depth of node in tree
+        self.cmass = pos # center of mass of node (start at center of node for now)
         #debug print(f"d{depth} Node: pos={self.pos}, size={self.size}, mass={self.mass}")
+    
+    
     def insert(self, particle: Particle) -> None:
-        self.particles.append(particle)
+        '''insert particle into node and update mass of node'''
+        self.particles.append(particle) 
         self.mass += particle.mass
         #debug print(f"inserted particle with mass {particle.mass} into node with mass {self.mass}")
 
+    
     def split(self) -> None:
-        if not self.is_end:
+        '''split node into 8 octants and redistribute particles to children nodes'''
+        if not self.is_end: # if it is already a leaf node, don't split
             #debug print(f"splitting a d{self.depth} Node: pos={self.pos}, size={self.size}, mass={self.mass}")
-            
             #split into 8 octants
             for i in [1,-1]:  # bisecting in each dimension
                 for j in [1,-1]:
                     for k in [1,-1]:
-                        pos = self.pos + np.array([i, j, k]) * self.size / 2
+                        pos = self.pos + np.array([i, j, k]) * self.size / 2 # calculate child node position
                         self.children.append(Node(pos, self.size/2, depth=self.depth+1))  # add as child node
-            # redistribute particles to children
-            for particle in self.particles: 
+            
+            for particle in self.particles: # redistribute particles to children nodes
                 for child in self.children:  
-                    if child.contains(particle):
+                    if child.contains(particle): # if particle is within child node, insert
                         child.insert(particle)
                         break
                 else:
                     raise ValueError(f"particle not in any child node: \n{particle}")
                     
-            self.children = [child for child in self.children if child.mass > 0]
-            self.particles = []  # clear particles list after redistribution
+            self.children = [child for child in self.children if child.mass > 0] # get rid of empty nodes
+            self.particles = []  # clear particles list after redistribution, since they are now in children
             for child in self.children:
-                if len(child.particles) > 1:
+                if len(child.particles) > 1: # if child has more than one particle, split again
                     child.split()
-                    self.fast_cmass()
+                    self.fast_cmass() # update center of mass of parent node
                 else:
-                    child.is_end = True
+                    child.is_end = True # if child has only one particle, it is a leaf node
+    
+    
     def contains(self, particle: Particle) -> bool:
-        return np.all(np.abs(particle.pos - self.pos) <= self.size)   
+        """check if particle is within node's volume"""
+        return np.all(np.abs(particle.pos - self.pos) <= self.size)  # check if particle is within node's volume by comparing each dimension 
+    
     
     def fast_cmass(self) -> np.ndarray:
-        self.cmass = np.sum([p.mass*p.pos for p in [*self.particles,*self.children]], axis=0) / self.mass
+        """calculate center of mass of node using fast method"""
+        self.cmass = np.sum([p.mass*p.pos for p in [*self.particles,*self.children]], axis=0) / self.mass # sum of mass*position / total mass. faster because we remove most children before using it.
         return self.cmass
-    def __repr__(self) -> str:
+    
+    
+    def __repr__(self) -> str: # a debug representation of the node containing its children's and particles' info recursively
         return (f"""
 d{self.depth}Node: pos={self.pos} size={self.size} mass={self.mass} 
 children({len(self.children)}): {[child for child in self.children]}
 particles({len(self.particles)}): {[particle for particle in self.particles]}""")
 
-    def treeview(self) -> str:
+
+    
+
+    def treeview(self) -> str: # recursive representation of the tree
+        """print tree structure of node and children recursively"""
         bars = "   ⎸"*self.depth
         info = f"⟶  d{self.depth} Node[{len(self.children)}ch,{len(self.particles)}p], cmass {self.cmass}, Mass:{self.mass}"
         if not self.is_end:
@@ -113,57 +146,45 @@ particles({len(self.particles)}): {[particle for particle in self.particles]}"""
         for c in self.children:
             c.treeview()
 
-    def dist(self, other):
+    
+    def dist(self, other) -> np.ndarray: 
+        """calculate distance between center of masses of two nodes"""
         return np.linalg.norm(self.cmass - other.cmass)
+
+##########################################################################################
+#                                        BHTree class                                    #
+##########################################################################################
+
 class BHTree:
-    def __init__(self, particles, origin:np.ndarray=np.zeros(3), size:float=100.) -> None:
-        [np.mean(comp) for comp in zip(*[p.pos for p in particles])]
-        #initpos = np.array([np.mean(comp) for comp in zip(*[p.pos for p in particles])])
-        self.root = Node(origin, #initpos, 
-                         size #np.max([np.max([np.abs(particle.pos - initpos)]) for particle in particles])
-                         )
+    def __init__(self, particles, theta:float=0.5, origin:np.ndarray=np.zeros(3), size:float=100.) -> None:
+        self.theta = theta
+        #debug [np.mean(comp) for comp in zip(*[p.pos for p in particles])]
+        #debug initpos = np.array([np.mean(comp) for comp in zip(*[p.pos for p in particles])])
+        self.root = Node(
+            pos= origin, size = size    #commented out bits are for a case where we might not know the size of the system initially
+            #debug pos = initpos,
+            #debug size = np.max([np.max([np.abs(particle.pos - initpos)]) for particle in particles])
+            )
         for particle in particles:
-            self.root.insert(particle)
-        self.root.split()
+            self.root.insert(particle) # insert particles into root node 
+        self.root.split() # begin recursive splitting of nodes
+
 
         
         
-    # barnes hut algorithm
 
-    # 1, build octree by
-        # defining a boundary volume
-        # split volume into 8 octants
-        # if octant contains >1 particles, split again, otherwise leave octant as leaf
-        # continue until all octants contain 0 or 1 particles  
+    #?####################################################################################    
 
-
-'''
-Calculating the force acting on a body
-To calculate the net force on a particular body, the nodes of the tree are traversed, starting from the root.
-If the center of mass of an internal node is sufficiently far from the body, the bodies contained in that part
-of the tree are treated as a single particle whose position and mass is respectively the center of mass and total
-mass of the internal node. If the internal node is sufficiently close to the body, the process is repeated for
-each of its children.
-
-Whether a node is or isn't sufficiently far away from a body, depends on the quotient s/d where s is the width
-of the region represented by the internal node, and d is the distance between the body and the node's center of
-mass. The node is sufficiently far away when this ratio is smaller than a threshold value θ. The parameter θ
-determines the accuracy of the simulation; larger values of θ increase the speed of the simulation but
-decreases its accuracy. If θ = 0, no internal node is treated as a single body and the algorithm degenerates
-to a direct-sum algorithm.
-
-
-start at root node
-if node is external, calculate force exerted by node on body
-    if node is internal, calculate s/d
-        if s/d < θ, treat internal node as single body, calculate force
-        if s/d > θ, recursively apply Barnes-Hut to each child node
-        add forces together
-'''
+##########################################################################################
+#                                      Engine class                                      #
+########################################################################################## 
 
 
 
 
+##########################################################################################
+#                                           Main                                         #
+########################################################################################## 
 
 
 # create some particles
